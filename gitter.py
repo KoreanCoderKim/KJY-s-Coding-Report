e6cdf13dc73b4736fb2230ca86b89d4dd1bf7131
KimJY <sagwai141@gmail.com>
2025-08-19 16:48:35 +0900

    gitter.py 업데이트

--- gitter.py
+++ gitter.py
@@ -1,6 +1,13 @@
 class super:
     functions = []
+    funced = []
     def run(self, idx, *args, **kwargs):
-        return self.functions[idx](*args, **kwargs)
+        for f in self.functions:
+           try:
+             f(*args,**kwargs)
+              self.funced.append(str(f))
+            except Exception as e:
+              pass
+        return self.funced
     def this(self, func):
         self.functions.append(func)
