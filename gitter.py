6facb1d8705f3322004888f943eb9b7502bf15ce
KimJY <sagwai141@gmail.com>
2025-08-19 16:31:24 +0900

    gitter.py 업데이트

--- gitter.py
+++ gitter.py
@@ -1,11 +1,6 @@
 class super:
     functions = []
-    def run(self, idx, v):
-        return self.functions[idx](v)
+    def run(self, idx, *args, **kwargs):
+        return self.functions[idx](*args, **kwargs)
     def this(self, func):
         self.functions.append(func)
-
-S = super()
-S.this(type)
-S.this(print)
-S.run(1, S.run(0, "str"))
